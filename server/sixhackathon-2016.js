2e51b201f5fb24f4a19ce0b89c914d70fa18bf98
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-05 20:36:49 +0100

    managing trend files

--- server/sixhackathon-2016.js
+++ server/sixhackathon-2016.js
@@ -27,26 +27,32 @@ Meteor.methods({
 });
 
 Meteor.startup(function () {
-    var fileContents = Assets.getText('trends.csv');
-    var fileContentsArray = fileContents.split(/\r?\n/);
+    for (var i = 1; i < 100; i++) {
+        try {
+            var fileContents = Assets.getText('trends' + i + '.csv');
+        } catch (err) {
+            break;
+        }
+        var fileContentsArray = fileContents.split(/\r?\n/);
 
-    var line = fileContentsArray[0];
-    var trendArray = line.split(',');
-    for (var j = 1; j < trendArray.length; j++) {
-        if (!trends.findOne({
-                trend: trendArray[j]
-            })) {
-            for (var i = 1; i < fileContentsArray.length; i++) {
-                var line = fileContentsArray[i];
-                var values = line.split(',');
-                //console.log(values);
-                trends.insert({
-                    trend: trendArray[j],
-                    date: new Date(values[0].substring(0, 10)),
-                    value: parseFloat(values[j])
-                });
+        var line = fileContentsArray[0];
+        var trendArray = line.split(',');
+        for (var j = 1; j < trendArray.length; j++) {
+            if (!trends.findOne({
+                    trend: trendArray[j]
+                })) {
+                for (var i = 1; i < fileContentsArray.length; i++) {
+                    var line = fileContentsArray[i];
+                    var values = line.split(',');
+                    //console.log(values);
+                    trends.insert({
+                        trend: trendArray[j],
+                        date: new Date(values[0].substring(0, 10)),
+                        value: parseFloat(values[j])
+                    });
+                }
+                console.log("loading " + fileContentsArray.length + " " + trendArray[j]);
             }
-            console.log("loading " + fileContentsArray.length + " " + trendArray[j]);
         }
     }
 });
