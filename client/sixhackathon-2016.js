d972631d4a648b55a30fc42bb21ff839b9d9d54b
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-03-06 10:15:31 +0100

    site lookin ok...

--- client/sixhackathon-2016.js
+++ client/sixhackathon-2016.js
@@ -1,3 +1,11 @@
+//$(window).load(function () { // makes sure the whole site is loaded
+//    $('#status').fadeOut(); // will first fade out the loading animation
+//    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+//    $('body').delay(350).css({
+//        'overflow': 'visible'
+//    });
+//})
+
 Session.set('trend', '');
 setTrend = function (newTrend) {
     Session.set('trend', newTrend);
